50aa8ad989f97b27d042b8f66972f6d993158434
Josê de Melo Figueiredo Boer <202504441352@alunos.estacio.br>
2026-02-06 22:17:32 +0000

    Adicionei a função de ataque e de sorteio de dados com rand.

--- war.c
+++ war.c
@@ -1,25 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h> 
 #include <string.h>
+#include <time.h> // Para os dados funcionarem
 
-// Aqui eu digo como é um território
 typedef struct {
     char nomeLocal[30];
     char corTime[10];
     int qtdTropas;
 } Territorio;
 
+// FUNÇÃO DE ATAQUE: Onde a mágica da batalha acontece
+void simularAtaque(Territorio *atacante, Territorio *defensor) {
+    int dadoAtaque = (rand() % 6) + 1; // Sorteia 1 a 6
+    int dadoDefesa = (rand() % 6) + 1;
+
+printf("\nBatalha: %s vs %s\n", atacante->nomeLocal, defensor->nomeLocal);
+    printf("Dados: Atacante %d | Defensor %d\n", dadoAtaque, dadoDefesa);
+
+    // Se o atacante ganhar ou empatar
+    if (dadoAtaque >= dadoDefesa) {
+        printf("O atacante ganhou! O defensor perdeu 1 tropa.\n");
+        defensor->qtdTropas--;
+
+        // Se o defensor ficar sem nada, o atacante ganha o lugar
+        if (defensor->qtdTropas <= 0) {
+            printf("VITÓRIA! %s conquistou o territorio!\n", atacante->nomeLocal);
+            strcpy(defensor->corTime, atacante->corTime);
+            defensor->qtdTropas = 1;
+        }
+    } else {
+        printf("A defesa ganhou! O atacante perdeu 1 tropa.\n");
+        if (atacante->qtdTropas > 1) atacante->qtdTropas--;
+    }
+}
+
 int main() {
     int n, i;
     Territorio *meusTerritorios;
+    srand(time(NULL)); // Liga o sorteio de dados
 
     printf("Quantos territorios voce quer cadastrar? ");
     scanf("%d", &n);
 
-    // CALLOC: Pede um espaço na memória para guardar os territórios
     meusTerritorios = (Territorio *)calloc(n, sizeof(Territorio));
 
-    // Laço para perguntar o nome de cada um
     for (i = 0; i < n; i++) {
         printf("\nNome do %d o lugar: ", i + 1);
         scanf(" %[^\n]s", meusTerritorios[i].nomeLocal);
@@ -29,10 +53,20 @@ int main() {
         scanf("%d", &meusTerritorios[i].qtdTropas);
     }
 
-    printf("\nPronto! Tudo guardado na memoria.\n");
+    // Agora vamos brincar de atacar!
+    int at, df;
+    printf("\nEscolha o numero do Atacante (1 a %d): ", n);
+    scanf("%d", &at);
+    printf("Escolha o numero do Defensor (1 a %d): ", n);
+    scanf("%d", &df);
 
-    // FREE: Devolve o espaço que usamos
+    // Faz a batalha acontecer usando os endereços (&)
+    simularAtaque(&meusTerritorios[at-1], &meusTerritorios[df-1]);
+
+    // Devolve a memória no final
     free(meusTerritorios);
+    printf("\nTrabalho finalizado e memoria limpa!\n");
     
     return 0;
 }
+
